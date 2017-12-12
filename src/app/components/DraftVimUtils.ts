ce659862ed9b56c650d610b72c18ebe8e7f684cb
Albert Lockett <alockett@ca.ibm.com>
2017-12-11 21:37:16 -0400

    added commands for j and k

--- src/app/components/DraftVimUtils.ts
+++ src/app/components/DraftVimUtils.ts
@@ -13,7 +13,18 @@ type VimEditorState = {
 
 const Keys = {
   h: 72,
-  i: 73
+  i: 73,
+  j: 74,
+  k: 75,
+  l: 76
+};
+
+const Commands = {
+  CHAR_LEFT: 'char-left',
+  CHAR_RIGHT: 'char-right',
+  LINE_DOWN: 'line-down',
+  LINE_UP: 'line-up',
+  INSERT: 'insret'
 };
 
 
@@ -22,7 +33,7 @@ export const handleKeyCommand: (
 ) => VimEditorState | void = (command, vimState) => {
   
 
-  if(command === 'char-left') {
+  if(command === Commands.CHAR_LEFT) {
     if(vimState.commandMode) {
       return moveCursor({ x: -1 }, vimState);
     } else {
@@ -30,12 +41,37 @@ export const handleKeyCommand: (
     }
   }
 
-  if(command === 'insert') {
+  if(command === Commands.CHAR_RIGHT) {
+    if(vimState.commandMode) {
+      return moveCursor({ x: 1 }, vimState);
+    } else {
+      return insertCharacter('l', vimState);
+    }
+  }
+
+  if(command === Commands.INSERT) {
 
     if(vimState.commandMode) {
       return { ...vimState, commandMode: false };
     } else {
       return insertCharacter('i', vimState);
+    }
+  }
+
+  if(command === Commands.LINE_DOWN) {
+    if(vimState.commandMode) { 
+      return moveCursor({ x: 0, y: 1 }, vimState);
+    } else {
+      return insertCharacter('j', vimState);
+    }
+  }
+
+  if(command === Commands.LINE_UP) {
+    if(vimState.commandMode) {
+      console.log('here');
+      return moveCursor({ x: 0, y: -1 }, vimState);
+    } else {
+      return insertCharacter('k', vimState);
     }
   }
   
@@ -86,10 +122,19 @@ export const keyBindingFn: (e) => string = (event) => {
   switch(event.keyCode) {
 
     case Keys.h:
-      return 'char-left';
+      return Commands.CHAR_LEFT;
 
     case Keys.i: 
-      return 'insert';
+      return Commands.INSERT;
+
+    case Keys.j:
+      return Commands.LINE_DOWN;  
+
+    case Keys.k:
+      return Commands.LINE_UP;
+
+    case Keys.l:
+      return Commands.CHAR_RIGHT;
     
     default:
       return getDefaultKeyBinding(event);
@@ -98,7 +143,8 @@ export const keyBindingFn: (e) => string = (event) => {
 
 
 type CursorDiff = {
-  x: number; // horizontal movement within editor (< 0 = left, > 0 = right)
+  x?: number; // horizontal movement within editor (< 0 = left, > 0 = right)
+  y?: number;
 };
 export const moveCursor: (
   c: CursorDiff, v: VimEditorState
@@ -107,8 +153,30 @@ export const moveCursor: (
   let editorState = vimState.editorState;
   let selection = editorState.getSelection();
   let focusOffset = selection.getFocusOffset();
+  let content = editorState.getCurrentContent();
 
+  // adjust selection for y
+  if(cursorDiff.y && cursorDiff.y !== 0) {
+    let selectedBlock = content.getBlockForKey(selection.getFocusKey());
+    for(let yOffset = 0; yOffset < Math.abs(cursorDiff.y); yOffset++) {
+      let nextSelectedBlock;
+      if(cursorDiff.y < 0) {
+        nextSelectedBlock = content.getBlockBefore(selectedBlock.getKey());
+      } else {
+        nextSelectedBlock = content.getBlockAfter(selectedBlock.getKey());
+      }
+      if(nextSelectedBlock) { selectedBlock = nextSelectedBlock; }
+    }
+    selection = selection.merge({
+      focusKey: selectedBlock.getKey(),
+      anchorKey: selectedBlock.getKey()
+    });
+  }
+
+  // adjust selection for X
+  let focusedBlock = content.getBlockForKey(selection.getFocusKey());
   let nextFocusOffset = Math.max(0, focusOffset + cursorDiff.x);
+  nextFocusOffset = Math.min(nextFocusOffset, focusedBlock.getLength());
   selection = selection.merge({ 
     anchorOffset: nextFocusOffset, 
     focusOffset: nextFocusOffset 
